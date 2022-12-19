3989efa90fa6be7d662bcc9cf8b1b7a023f4d8b6
hathateth <albertoslavica19@gmail.com>
2022-12-19 22:02:05 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+
+fn main() {
+    println!("Write the username");
+    let mut _repo_name = String::new();
+
+    io::stdin()
+        .read_line(&mut _repo_name)
+        .expect("Failed to read line");
+        
+    println!("{_repo_name}");
+    
+    let url = format!("https://github.com/{}?tab=repositories", _repo_name.trim());
+
+    let response = reqwest::blocking::get(
+        url
+        )
+        .unwrap()
+        .text()
+        .unwrap();
+        
+        let mut repo_name = String::new();
+
+        let document = scraper::Html::parse_document(&response);
+        
+        let repo_selector = scraper::Selector::parse("#user-repositories-list>ul>li").unwrap();
+
+        let repos = document.select(&repo_selector).map(|x| x.inner_html());
+
+        repos
+            .zip(1..101)
+            .for_each(|(item, number)| println!("{}. {}", number, item));
+
+}
+
